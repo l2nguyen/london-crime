0fe7531b5ee110d071b5332c413cb8e5feb2adfe
Lena Nguyen <lena.nguyen4@gmail.com>
2018-12-20 12:34:35 +0000

    Changed code to get crime data into a function

--- get_bqdata.R
+++ get_bqdata.R
@@ -3,7 +3,13 @@ library('jsonlite')
 library('here')
 library('readr')
 
-# Gets data from Google BigQuery and writes to csv
+# Function to get crime data from Google BigQuery and writes to csv
+get_crimedata <- function(query, file = '') {
+
+# check filename was specified
+if (file == ''){
+  stop('Specify a file name.')
+}
 
 # specify project
 project <- fromJSON(here('auth_details.json'))$project_id
@@ -11,17 +17,21 @@ project <- fromJSON(here('auth_details.json'))$project_id
 # set google api authentication detials
 set_service_token(here('auth_details.json'))
 
+# Query the database
+table <- bq_project_query(project, query)
+# download query result into table
+query_result <- bq_table_download(table)
+
+# write table to csv
+write_csv(query_result, here(file))
+}
+
 # SQL query
-sql <- '
+sql_query <- '
 SELECT borough, major_category, SUM(value) AS num_crime, year, month
 FROM `bigquery-public-data.london_crime.crime_by_lsoa`
 GROUP BY borough, major_category, year, month
 ORDER BY borough, year, month, major_category
 '
 
-# Query the database
-table <- bq_project_query(project, sql)
-# download query result into table
-ldn_crime <- bq_table_download(table)
-# write table to csv
-write_csv(ldn_crime, here('ldn_crime.csv'))
+get_crimedata(sql_query, file = 'ldn_crime.csv')
